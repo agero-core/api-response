3c00edbcb6e82accfedc878adae0a8b793520b6a
Andrey Sergeev <andrey@chyuck.com>
2019-04-18 17:26:17 -0400

    Added .net core web test project

---
+++ Agero.Core.ApiResponse.Web.Core/Controllers/ResponseController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Agero.Core.ApiResponse.Exceptions;
+
+namespace Agero.Core.ApiResponse.Web.Core.Controllers
+{
+    [Route("responses")]
+    [ApiController]
+    public class ResponseController : ControllerBase
+    {
+        [Route("applicationError")]
+        [HttpGet]
+        public ObjectResult GetApplicationErrorResponse()
+        {
+            // See debug logs
+            throw new Exception("Application error.");
+        }
+
+        [Route("validationException")]
+        [HttpGet]
+        public ObjectResult GetValidationErrorResponse()
+        {
+            // See debug logs
+            throw new BadRequestException(
+                message: "Validation error.",
+                code: "VALIDATION_ERROR",
+                additionalData: new { status = "validation_error" });
+        }
+    }
+}
---
+++ Agero.Core.ApiResponse.Web.Core/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Agero.Core.ApiResponse.Extensions;
+using Agero.Core.ApiResponse.Filters;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace Agero.Core.ApiResponse.Web.Core
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var responseHandler =
+                new ResponseHandler(
+                    logInfo: (message, data) => Debug.WriteLine($"INFO: {message}{Environment.NewLine}{JsonConvert.SerializeObject(data)}"),
+                    logError: (message, data) => Debug.WriteLine($"ERROR: {message}{Environment.NewLine}{JsonConvert.SerializeObject(data)}"),
+                    extractAdditionalData: ex => ex.ExtractAdditionalData(),
+                    includeExceptionDetails: true);
+
+            services.AddMvc(config =>
+            {
+                config.Filters.Add(new ExceptionHandlingFilterAttribute(responseHandler));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.Use(middleware => context =>
+            {
+                context.Request.EnableBuffering();
+
+                return middleware(context);
+            });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
